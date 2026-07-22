b7e8b6b2afc065bc17e774b9420bafbcfd64e4f5
Andrew Murray <darbyguitars@gmail.com>
2026-07-22 14:42:49 -0500

    create get route for get all games

--- src/handlers.rs
+++ src/handlers.rs
@@ -47,3 +47,26 @@ pub async fn create_game_handler(
 
     Ok(Json(game_response))
 }
+
+pub async fn game_list_handler(
+    State(data): State<Arc<AppState>>,
+) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
+    let games = sqlx::query_as!(GameModel, r#"SELECT * FROM games ORDER by name"#)
+        .fetch_all(&data.db)
+        .await
+        .map_err(|e| {
+            let error_response = serde_json::json!({
+                "status": "error",
+                "message" : format!("Database error: {}", e)
+            });
+            (StatusCode::INTERNAL_SERVER_ERROR, Json(error_response))
+        })?;
+
+    let json_response = serde_json::json!({
+        "status": "ok",
+        "count" : games.len(),
+        "notes": games
+    });
+
+    Ok(Json(json_response))
+}
--- src/routes.rs
+++ src/routes.rs
@@ -1,10 +1,10 @@
-use axum::{Router, routing::post};
+use crate::{AppState, handlers::create_game_handler, handlers::game_list_handler};
+use axum::{Router, routing::get, routing::post};
 use std::sync::Arc;
-
-use crate::{AppState, handlers::create_game_handler};
 
 pub fn create_router(app_state: Arc<AppState>) -> Router {
     Router::new()
         .route("/api/game", post(create_game_handler))
+        .route("/api/game", get(game_list_handler))
         .with_state(app_state)
 }
