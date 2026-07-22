17551936fac55c07013b6a5b69e3f59b83dabf48
Andrew Murray <darbyguitars@gmail.com>
2026-07-22 10:15:12 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use axum::{Router, routing::get};
+mod routes;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/api", get(routes::hello_world));
+
+    // listen globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Server started successfully at 0.0.0.0:3000");
+    axum::serve(listener, app).await.unwrap();
+}
